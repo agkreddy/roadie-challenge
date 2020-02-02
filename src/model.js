40e327cb0c76e444f55229ad67aca64266a242d5
Gopala <agkreddy@gmail.com>
2020-02-02 15:49:36 +0530

    2nd version dyanmic

--- src/model.js
+++ src/model.js
@@ -1,73 +1,25 @@
+import StarRatings from './react-star-ratings';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
- 
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+class Foo extends React.Component {
+  changeRating( newRating, name ) {
+    this.setState({
+      rating: newRating
+    });
   }
-};
- 
-// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement('#root')
- 
-class App extends React.Component {
-  constructor() {
-    super();
- 
-    this.state = {
-      modalIsOpen: false
-    };
- 
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
- 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
- 
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
- 
-  closeModal() {
-    this.setState({modalIsOpen: false});
-  }
- 
+
   render() {
+    // rating = 2;
     return (
-      <div>
-        <button onClick={this.openModal}>Open Modal</button>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
- 
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
-        </Modal>
-      </div>
+      <StarRatings
+        rating={this.state.rating}
+        starRatedColor="blue"
+        changeRating={this.changeRating}
+        numberOfStars={6}
+        name='rating'
+      />
     );
   }
 }
- 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Foo />, document.getElementById('row'));
